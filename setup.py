aa3f335a304a6511b887df529fff992d9368a159
Samuel Farrens <samuel.farrens@gmail.com>
2017-12-06 17:15:54 +0100

    updated download URL

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='samuel.farrens@gmail.com',
     version=__version__,
     url='https://github.com/sfarrens/sf_tools',
-    download_url='https://github.com/sfarrens/sf_tools/archive/0.1.tar.gz',
+    download_url='https://github.com/sfarrens/sf_tools',
     packages=find_packages(),
     license='MIT',
     description='Tools for image analysis, signal processing and statistics.',
