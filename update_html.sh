e71c518a86bdf78e3d660edc05972c1ab82d7b02
Samuel Farrens <samuel.farrens@gmail.com>
2017-12-06 18:01:51 +0100

    updated html

--- update_html.sh
+++ update_html.sh
@@ -1,6 +1,6 @@
 rm -rf *.html _sources _modules _static
-git checkout master sf_tools/docs/build/html
-mv sf_tools/docs/build/html/* .
+git checkout master docs/build/html
+mv docs/build/html/* .
 rm -r docs
 git add .
 git commit -m "updated html"
