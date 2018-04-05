efd9dbd98cc06c9f140d0dc10d9300dcc469381e
Samuel Farrens <samuel.farrens@gmail.com>
2018-04-05 11:47:22 +0200

    cleaned up sf_tools

--- setup.py
+++ setup.py
@@ -1,17 +1,27 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from setuptools import setup, find_packages
-from sf_tools import __version__
+import os
+
+release_info = {}
+infopath = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                           "sf_tools", "info.py"))
+with open(infopath) as open_file:
+    exec(open_file.read(), release_info)
 
 setup(
     name='sf_tools',
     author='sfarrens',
-    author_email='samuel.farrens@gmail.com',
-    version=__version__,
+    author_email='samuel.farrens@cea.fr',
+    version=release_info["__version__"],
     url='https://github.com/sfarrens/sf_tools',
     download_url='https://github.com/sfarrens/sf_tools',
     packages=find_packages(),
+    install_requires=['numpy>=1.14.1', 'future>=0.16.0', 'scipy>=1.0.0', 'astropy==2.0.4'],
     license='MIT',
     description='Tools for image analysis, signal processing and statistics.',
-    long_description=open('README.txt').read(),
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest'],
+    long_description=release_info["__about__"],
+    setup_requires=['pytest-runner', ],
+    tests_require=['pytest', 'pytest-cov', ],
 )
--- sf_tools/image/__init__.py
+++ sf_tools/image/__init__.py
@@ -6,10 +6,6 @@ This module contains submodules for image analysis.
 
 :Author: Samuel Farrens <samuel.farrens@gmail.com>
 
-:Version: 1.0
-
-:Date: 06/04/2017
-
 References
 ----------
 
@@ -28,6 +24,6 @@ References
 
 """
 
-__all__ = ['convolve', 'distort', 'quality', 'shape', 'stamp', 'stats']
+__all__ = ['distort', 'quality', 'shape', 'stamp', 'stats']
 
 from . import *
